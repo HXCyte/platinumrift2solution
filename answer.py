3626387425f586f030fb953f6f010cfb8251ac96
HXCyte <36270245+HXCyte@users.noreply.github.com>
2019-02-22 15:35:02 +0700

    changed from link-based to zone-based, added class Zone

--- answer.py
+++ answer.py
@@ -1,55 +1,64 @@
 import sys
-import math
+import math 
 import random
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+# Class Zone represents a zone.
+class Zone:
+    def __init__ (self): # constructor
+        self.owner = 0
+        self.p0 = 0
+        self.p1 = 0
+        self.vis = 0
+        self.plt = 0
+    def upd(self, o, p1, p2, v, p): # update value
+        self.owner = o
+        self.p0 = p1
+        self.p1 = p2
+        self.vis = v
+        self.plt = p
+
+def strategy(my_pods, enemy_pods, z_id, neighbor): # strategy
+    return 0
 
 # player_count: the amount of players (always 2)
 # my_id: my player ID (0 or 1)
 # zone_count: the amount of zones on the map
 # link_count: the amount of links between all zones
+# zones: contains all information for zones
+# neighborZones : ID of neighboring zones
 player_count, my_id, zone_count, link_count = [int(i) for i in raw_input().split()]
-availableLinks = []
-links = []
+zones = []
+neighborZones = []
 for i in xrange(zone_count):
     # zone_id: this zone's ID (between 0 and zoneCount-1)
     # platinum_source: Because of the fog, will always be 0
     zone_id, platinum_source = [int(j) for j in raw_input().split()]
-    availableLinks.append([])
+    zones.append(Zone())
+    neighborZones.append([])
 for i in xrange(link_count):
     zone_1, zone_2 = [int(j) for j in raw_input().split()]
-    links.append([zone_1, zone_2])
-    availableLinks[zone_1].append(i)
-    availableLinks[zone_2].append(i)
+    neighborZones[zone_1].append(zone_2)
+    neighborZones[zone_2].append(zone_1)
+    
 # game loop
 while True:
     comm = ""
     my_platinum = int(raw_input())  # your available Platinum
     for i in xrange(zone_count):
-        # z_id: this zone's ID
-        # owner_id: the player who owns this zone (-1 otherwise)
-        # pods_p0: player 0's PODs on this zone
-        # pods_p1: player 1's PODs on this zone
-        # visible: 1 if one of your units can see this tile, else 0
-        # platinum: the amount of Platinum this zone can provide (0 if hidden by fog)
         z_id, owner_id, pods_p0, pods_p1, visible, platinum = [int(j) for j in raw_input().split()]
-        if my_id == 0:
-            my_pods = pods_p0
+        zones[z_id].upd(owner_id, pods_p0, pods_p1, visible, platinum)
+        
+    for i in xrange(zone_count):
+        my_zone = zones[i]      # get zone
+        if my_id == 0:          # owned pod in the zone
+            my_pods = my_zone.p0
         else:
-            my_pods = pods_p1
+            my_pods = my_zone.p1
         if my_pods > 0:
-            comm += str(my_pods)
-            selectedLink = links[availableLinks[i][random.randrange(0, len(availableLinks[i]))]]
-            if selectedLink[0] == i:
-                comm += " " + str(selectedLink[0]) + " " + str(selectedLink[1]) + " "
-            else:
-                comm += " " + str(selectedLink[1]) + " " + str(selectedLink[0]) + " " 
-    # Write an action using print
-    # To debug: print >> sys.stderr, "Debug messages..."
-
-
-    # first line for movement commands, second line no longer used (see the protocol in the statement for details)
+            # execute command(s)
+            comm += str(my_pods) + " "
+            comm += str(i) + " "
+            comm += str(neighborZones[i][random.randrange(len(neighborZones[i]))]) + " "
 
     print (comm)
     print "WAIT"
